512dcb31a8e74b2a99985db86d6ec4b9f2e69a0d
ArthurDaniel40 <arthurfico56@gmail.com>
2024-08-20 10:34:44 -0300

    player

--- Meu Game 3D/Assets/New Folder/player.cs
+++ Meu Game 3D/Assets/New Folder/player.cs
@@ -1,27 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
-using JetBrains.Annotations;
-using UnityEditor.Callbacks;
 using UnityEngine;
 
-public class player : MonoBehaviour
+public class Player : MonoBehaviour
 {
     public int velocidade = 10;
-    public Rigidbody rigido;
+    public Rigidbody  rb;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("iniciar");
-        TryGetComponent(out rigido);
+        Debug.Log("hello mundo");
+        TryGetComponent(out rb);
     }
 
     // Update is called once per frame
     void Update()
     {
-       Debug.Log("update"); 
-       float x = Input.GetAxis("horizontal");
-       float y = Input.GetAxis("Vertical");
-       UnityEngine.Vector3 Direcao = new UnityEngine.Vector3(x, 0, y);
-       rigido.AddForce(Direcao * velocidade * Time.deltaTime, ForceMode.Impulse);
+        Debug.Log("update");
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        Vector3 direcao = new Vector3(x, 0, y);
+        rb.AddForce(direcao * velocidade * Time.deltaTime, ForceMode.Impulse);
     }
 }
